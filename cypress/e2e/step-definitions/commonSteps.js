52ff5f96171324f199d5d279049ec522e104acc9
alejandrojavier.peco.tech <alejandrojavier.peco.tech@bbva.com>
2024-06-24 15:57:25 +0200

    first commit

---
+++ cypress/e2e/step-definitions/commonSteps.js
@@ -0,0 +1,87 @@
+import 'cypress-mochawesome-reporter/cucumberSupport';
+import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
+import { CommonPage } from "../pages/commonPage"
+
+let commonPage = new CommonPage();
+
+Given("I visit {string}", (url) => {
+  commonPage.visitLink(url);
+});
+
+When("I check that the page should {string} the text {string}", (status, text) => {
+  commonPage.checkBodyText(status, text);
+  // Status accept the values: contain , not.contain
+})
+
+Given("I check that the url value contain the text {string}", (urlValue) => {
+  commonPage.checkUrlValue(urlValue);
+})
+
+Then("I check that the url value not contain the text {string}", (urlValue) => {
+  commonPage.checkUrlNotValue(urlValue);
+})
+
+When("I check that the url value {string} is {string}", (urlValue, status) => {
+  commonPage.checkUrlValueStatus(status, urlValue)
+  // status value must be : include or not include
+})
+
+When("I check that the status is {string} with the  url value {string}", (status, urlValue) => {
+  commonPage.checkUrlValueStatus(status, urlValue)
+  // status value must be : include or not include
+})
+
+Given("I check that the element with data test {string} should have the status {string}", (elementDataTest, status) => {
+  commonPage.checkElementStateByDataTest(elementDataTest, status);
+})
+
+When("I check that the element with class {string} should have the content {string}", (className, content) => {
+  commonPage.checkContentByClass(className, content);
+})
+
+Given("I check that the element with data test {string} should have the value {string}", (elementDataTest, value) => {
+  commonPage.checkElementValueByDataTest(elementDataTest, value);
+})
+
+Given ("I visit my url", () => {
+  commonPage.visitBaseUrl()
+})
+
+Given ("I visit the saucedemo web page", () => {
+  commonPage.visitSauceDemo()
+})
+
+Given ("If the button {string} exist i click on it", (button) => {
+commonPage.clickButtonIfExist(button)
+})
+
+Then ('I click on the button named {string} and wait for api call {string}', (buttonName, apiCall) => {
+  commonPage.clickButtonByNameWaitCookies(buttonName, apiCall)
+})
+
+Then ('I click on the button named {string}', (buttonName) => {
+  commonPage.clickButtonByName(buttonName)
+})
+
+Then ('I wait {int} miliseconds', (seconds) => {
+  commonPage.waitXSeconds(seconds)
+})
+
+Then ('I intercept the api call {string} with the alias {string}', (apiCall, aliasApiCall) => {
+  commonPage.interceptApiCallAddAlias(apiCall, aliasApiCall)
+})
+
+Then ('I wait {int} miliseconds for the api call with the alias {string}', (time, aliasApiCall) => {
+  commonPage.waitApiCallByAlias(time, aliasApiCall)
+})
+
+// Step para test de accesibilidad
+Then ('I test the accesibility in all the screen', () => {
+  commonPage.testAccesibilityInScreen()
+})
+
+Then ('I test the accesibility on the element with locator {string}', (elementLocator) => {
+  commonPage.testAccesibilityOnElement(elementLocator)
+})
+
+
